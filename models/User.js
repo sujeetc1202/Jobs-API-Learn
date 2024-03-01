73831a4a53d3e45a6ad2efabf1c15f69baa56f51
Sujeet Kumar <sujeetc1202kc@gmail.com>
2024-03-01 15:39:00 +0530

    created instance with mongoose

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -29,5 +30,17 @@ UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// UserSchema.methods.getName = function () {
+//   return this.name;
+// };
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    { expiresIn: "30d" }
+  );
+};
 
 module.exports = mongoose.model("User", UserSchema);
