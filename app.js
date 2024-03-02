3ef41a0299871f71c413b0a0fdeed33afb71cfbe
Sujeet Kumar <sujeetc1202kc@gmail.com>
2024-03-02 16:47:23 +0530

    Implemented security setups

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require("dotenv").config();
 require("express-async-errors");
+
+
+// Security setup
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const rateLimiter = require("express-rate-limit")
+
 const express = require("express");
 const app = express();
 
@@ -18,7 +26,23 @@ const errorHandlerMiddleware = require("./middleware/error-handler.js");
 
 // middleware
 
+app.set("trust proxy", 1)
+
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Use an external store for consistency across multiple server instances.
+  })
+);
+
+
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 
